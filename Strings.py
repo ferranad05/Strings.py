a93df1aee2fb7dff957aa450cbdbfdce9fd570b8
Ferran Álvarez <87202859+ferranad05@users.noreply.github.com>
2021-07-09 20:54:16 +0200

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,32 @@
+print("Giraffe\nAcademy\n") #Jump to the next line the things that are after the \n
+########################################################################################################################
+print("\"Giraffe\" Academy\n") #Writes a "
+########################################################################################################################
+phrase = "Giraffe Academy"
+print(phrase+" is cool\n")
+########################################################################################################################
+phrase = "Giraffe Academy\n"
+print(phrase.lower()) #Converts the letters in lowercase
+########################################################################################################################
+phrase = "Giraffe Academy\n"
+print(phrase.upper()) #Converts the letters in uppercase
+########################################################################################################################
+phrase = "Giraffe Academy"
+print(phrase.isupper()) #This makes us know if that that we have write is True or False
+#We can also do this:
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper()) #Here first change the letters to uppercase and then says if they're in uppercase or not
+########################################################################################################################
+print(len(phrase)) #Tells us the name of characters
+########################################################################################################################
+phrase = "Giraffe Academy"
+print(phrase[0]) #Shows the letter in the position 0, we can modify this value to make the machine show us the letter that we want
+########################################################################################################################
+phrase = "Giraffe Academy"
+print(phrase.index("a")) #We write a letter and the machine tells us the position of that letter
+########################################################################################################################
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Ferry")) #Replace the string we have write for other that we want
+
+
+
